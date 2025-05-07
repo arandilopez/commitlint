78155a99a9c1910ff729e3f898d6c0d48f4496a4
Arandi Lopez <arandilopez.93@gmail.com>
2025-05-06 18:35:53 -0600

    chore: initial commit

---
+++ lib/commitlint/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Commitlint
+  VERSION = "0.1.0"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "commitlint"
+
+require "minitest/autorun"
